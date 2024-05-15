cd2084c6c4f1737a836787d20ae1193d789473ae
mannumishra <mannumishra2207@outlook.com>
2024-05-15 16:59:50 +0530

    add changes

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import about from '../../image/about-banner-bg.jpg'
+import about from '../../image/aboutus.webp'
 import "./About.css"
 const About = () => {
   useEffect(() => {
