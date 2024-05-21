4925fb73cfc2096d6f733c68b7b7823f7d039b2d
mannumishra <mannumishra2207@outlook.com>
2024-05-21 15:54:07 +0530

    add chnages

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -18,12 +18,12 @@ const Navbar = () => {
       <section>
         <div className='navbarfixed'>
           <div><i class="ri-smartphone-line remixicon" ></i>011 4076 4076</div>
-          <div className='d-flex g-5'>
-            <div><img src={india} alt="" style={{ height: 20 }} /> India</div>
-            <div><img src={koriya} alt="" style={{ height: 20 }} /> Koriya</div>
-          </div>
+
           <div><i class="ri-time-line remixicon"></i>  10:00 AM - 06:00 PM - Monday to Saturday</div>
-         
+          <div className='' style={{ display: "flex", gap: 20 }}>
+            <div><img src={india} alt="" style={{ height: 22 }} />  India</div>
+            <div><img src={koriya} alt="" style={{ height: 30 }} /> Koriya</div>
+          </div>
         </div>
         <div className="navbarmain">
           <div className="navlogo">
